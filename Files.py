e91c24a0cb96286b3b06c9d86e4c8aee04f5bb68
CRuby621 <Connorr_03@outlook.com>
2024-08-21 16:46:21 +1000

    Create Files.py

---
+++ Files.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 16:44:27 2024
+
+@author: conno
+"""
+
+filename = "file2.txt"
+
+# open file for writing
+file = open(filename, 'w')
+
+# write content
+file.write("WHAT!?")
+
+# close the file
+file.close()
+
+# Option 2
+with open(filename, 'w') as file:
+    file.write("This is content from with")
+    
+
+####
+
+# # Open a file
+# filename = "File1.txt"
+
+# # File_object = open(r"File_Name", "Access_Mode")
+
+# # f = open(r"C:\Documents\temp\filename.txt")
+# f = open(filename, "r")
+
+# # with open("file1.txt", w) as f1:
+# #     f1.write
+
+# # Read contents
+# print(f.read())
+
+
+# # Close file
+# f.close()
